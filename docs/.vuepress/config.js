3c7cba1eed2a4785bc7015e9d592c933a33a0f24
yanjiangtao <756994208@qq.com>
2019-07-07 11:11:52 +0800

    ele

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,10 @@
 module.exports = {
     title: 'Adroi媒体API 接口文档',  // 设置网站标题
     description : 'Adroi',
-    base : '/v1/adroi-h5/adroiapi/',
+    base : '/orangeCode/',
     themeConfig : {
       nav : [
-          { text: '接口定义', link: '/apiword' },
-          { text: '接口字段定义', link: '/api' },
-          { text: '附录：错误码', link: '/error' }
+          { text: '接口定义', link: '/apiword' }
       ],
       sidebar: {
           '/' : [
